48f83aebc34cd31fab43daafbd32ecb4c7ad9449
zl1nux <kenny.zk46@gmail.com>
2020-11-22 00:51:36 +0300

    website started

---
+++ chat/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+class IndexView(TemplateView):
+    template_name = 'chat/index.html'
+
+
+def room(request, room_name):
+    return render(request, 'chat/chatroom.html', {
+        'room_name': room_name
+    })
