de76214c1f975b03d54eb92c9d9039d4b8a65049
zl1nux <kenny.zk46@gmail.com>
2020-12-12 11:09:13 +0300

    for heroku

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,4 @@
 import json
-
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
@@ -7,6 +6,7 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -14,26 +14,33 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'tester_message',
-                'tester': 'hello world',
-
-            }
-        )
-
-    async def tester_message(self, event):
-        tester = event['tester']
-
-        await self.send(text_data=json.dumps({
-            'tester': tester,
-        }))
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chatroom_message',
+                'message': message,
+                'username': username,
+            }
+        )
+
+    async def chatroom_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+        }))
+
+    pass
--- projectmeme/settings.py
+++ projectmeme/settings.py
@@ -127,3 +127,13 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels.layers.InMemoryChannelLayer"
     },
 }
+# doesn't work
+
+# CHANNEL_LAYERS = {
+#     'default': {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         'CONFIG': {
+#             'hosts': [('127.0.0.1', 8001)]
+#         },
+#     },
+# }
